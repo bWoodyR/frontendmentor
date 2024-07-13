a935bea620031dbcb609e1e18954f78524c83702
bWoodyR <r.blackwoody@gmail.com>
2024-07-13 10:17:48 +0200

    adding .env to gitignore and fixing text color of button

--- components/ResultsSummaryComponent/SummaryButton.tsx
+++ components/ResultsSummaryComponent/SummaryButton.tsx
@@ -1,6 +1,6 @@
 const SummaryButton = () => {
   return (
-    <button className="w-full py-2 rounded-3xl bg-[#4d6aba] hover:bg-gradient-to-b from-[#7857ff] to-[#2e2be9]" style={{ backgroundColor: `hsla(${"224, 30%, 27%"})` }}>
+    <button className="w-full py-2 rounded-3xl bg-[#4d6aba] hover:bg-gradient-to-b from-[#7857ff] to-[#2e2be9] text-white" style={{ backgroundColor: `hsla(${"224, 30%, 27%"})` }}>
       Continue
     </button>
   );
