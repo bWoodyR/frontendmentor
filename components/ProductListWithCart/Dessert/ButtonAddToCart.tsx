fbedff39d49f8c438123db4fc62a1de05740ee6b
bWoodyR <r.blackwoody@gmail.com>
2024-07-15 21:38:28 +0200

    removing padding x from button

--- components/ProductListWithCart/Dessert/ButtonAddToCart.tsx
+++ components/ProductListWithCart/Dessert/ButtonAddToCart.tsx
@@ -8,7 +8,7 @@ type Props = {
 const ButtonAddToCart = ({ handleDessertClick }: Props) => {
   return (
     <button
-      className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 z-10 flex gap-2 items-center text-rose900 font-semibold text-sm bg-white px-6 py-2 rounded-3xl outline outline-2 outline-[#c9aea6] w-[155px] hover:text-dark-red hover:outline-dark-red transition-colors"
+      className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 z-10 flex gap-2 items-center justify-center text-rose900 font-semibold text-sm bg-white  py-2 rounded-3xl outline outline-2 outline-[#c9aea6] w-[155px] hover:text-dark-red hover:outline-dark-red transition-colors"
       onClick={() => handleDessertClick("add")}
     >
       <Image src={"/assets/images/product-list-with-cart/icon-add-to-cart.svg"} alt="add-to-cart-icon" width={0} height={0} className="w-5 h-5"  />
