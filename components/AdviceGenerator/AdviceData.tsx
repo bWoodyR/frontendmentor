1e8454bab75a1a82266ed480d02821d3e38835ee
bWoodyR <r.blackwoody@gmail.com>
2024-07-16 10:25:22 +0200

    adding quototation marks

--- components/AdviceGenerator/AdviceData.tsx
+++ components/AdviceGenerator/AdviceData.tsx
@@ -9,7 +9,7 @@ const AdviceData = ({ advice }: { advice: Advice | undefined }) => {
   return (
     <div className="flex flex-col gap-4 text-center items-center justify-center mb-8">
       <h2 className="text-neon-green text-sm tracking-[.25em]">ADVICE #{!advice || pending ? "" : advice?.slip?.id}</h2>
-      {!advice || pending ? <LoadingSpinner /> : <p className="flex items-center justify-center font-extrabold text-light-cyan">{`${advice?.slip?.advice}`}</p>}
+      {!advice || pending ? <LoadingSpinner /> : <p className="flex items-center justify-center font-extrabold text-light-cyan">{`“${advice?.slip?.advice}”`}</p>}
       <Image src="/assets/images/advice-generator/pattern-divider-desktop.svg" alt="pattern-devider" width={0} height={0} className="w-full h-6" />
     </div>
   );
