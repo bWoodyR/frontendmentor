8cf6e053875ca7ba683137504e91650e30b2e586
bWoodyR <r.blackwoody@gmail.com>
2024-07-16 10:42:36 +0200

    trivial fix - removing div from button element

--- components/AdviceGenerator/AdviceSubmitButton.tsx
+++ components/AdviceGenerator/AdviceSubmitButton.tsx
@@ -10,9 +10,7 @@ const AdviceSubmitButton = () => {
       disabled={pending}
       className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 z-10 bg-neon-green rounded-full w-[60px] h-[60px] flex items-center justify-center transition-all hover:cursor-pointer hover:shadow-neon-spread "
     >
-      <div>
-        <Image src={"/assets/images/advice-generator/icon-dice.svg"} alt="dice-icon" width={25} height={25}></Image>
-      </div>
+      <Image src={"/assets/images/advice-generator/icon-dice.svg"} alt="dice-icon" width={25} height={25}></Image>
     </button>
   );
 };
