caa5ea59e13ca30a8abb06370c7ba4abf4aa972a
bWoodyR <r.blackwoody@gmail.com>
2024-07-17 21:42:10 +0200

    hotFix, adding dynamic import for bg's, bacause of using screenSize hook

--- components/IPAddressTracker/IPAddressTracker.tsx
+++ components/IPAddressTracker/IPAddressTracker.tsx
@@ -1,12 +1,14 @@
 import IPForm from "./IPForm";
-import IPAddresssBackground from "./IPAddresssBackground";
 import { IPAddressProvider } from "./Context/IPAddressContext";
+import dynamic from "next/dynamic";
+
+const IPAddresssBackgroundDynamic = dynamic(() => import("@/components/IPAddressTracker/IPAddresssBackground"));
 
 const IPAddressTracker = async () => {
   return (
     <section className="relative flex flex-col  min-h-screen font-rubik">
       <IPAddressProvider>
-        <IPAddresssBackground />
+        <IPAddresssBackgroundDynamic />
         <IPForm />
       </IPAddressProvider>
     </section>
