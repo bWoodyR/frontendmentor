dadaa9a85bfd12c178b87f8c5ea1e30829b6f44b
bWoodyR <r.blackwoody@gmail.com>
2024-07-17 21:53:27 +0200

    changing to paragraphs

--- components/IPAddressTracker/SingleIPParam.tsx
+++ components/IPAddressTracker/SingleIPParam.tsx
@@ -6,8 +6,8 @@ type Props = {
 const SingleIPParam = ({ title, value }: Props) => {
   return (
     <div className=" flex flex-col items-center sm:items-start gap-2 xl:w-[175px] sm:pb-4">
-      <h5 className="text-dark-gray text-xs font-bold tracking-widest">{title}</h5>
-      <h2 className="text-black font-medium text-lg">{value}</h2>
+      <p className="text-dark-gray text-xs font-bold tracking-widest">{title}</p>
+      <p className="text-black font-medium text-lg">{value}</p>
     </div>
   );
 };
