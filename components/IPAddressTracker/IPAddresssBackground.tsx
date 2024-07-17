fb823d4d954d8854016fb631331a49cd02a98c7e
bWoodyR <r.blackwoody@gmail.com>
2024-07-17 21:34:36 +0200

    changing background image based on windowSize.width

--- components/IPAddressTracker/IPAddresssBackground.tsx
+++ components/IPAddressTracker/IPAddresssBackground.tsx
@@ -1,16 +1,27 @@
 "use client";
 
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { IPAddressContext } from "./Context/IPAddressContext";
 import LoadingSpinner from "../AdviceGenerator/LoadingSpinner";
+import { useScreenSize } from "@/hooks/useScreenSize";
 
 const IPAddresssBackground = () => {
   const { ipData } = useContext(IPAddressContext);
+  const [bgImg, setBgImge] = useState("bg-[url('/assets/images/ip-address-tracker/pattern-bg-desktop.png')]");
+  const { windowSize } = useScreenSize();
   const locality = ipData?.region;
+
+  useEffect(() => {
+    if (windowSize.width < 640) {
+      setBgImge("bg-[url('/assets/images/ip-address-tracker/pattern-bg-mobile.png')]");
+    } else {
+      setBgImge("bg-[url('/assets/images/ip-address-tracker/pattern-bg-desktop.png')]");
+    }
+  }, [windowSize.width]);
 
   return (
     <>
-      <div className="h-[275px] sm:h-[225px] bg-fixed bg-cover  bg-no-repeat bg-[url('/assets/images/ip-address-tracker/pattern-bg-desktop.png')]"></div>
+      <div className={`h-[275px] sm:h-[225px] bg-fixed bg-cover  bg-no-repeat ${bgImg}`}></div>
       {locality ? (
         <iframe src={`https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}&q=${locality}&zoom=16&maptype=satellite`} className="w-full min-h-max flex-1"></iframe>
       ) : (
